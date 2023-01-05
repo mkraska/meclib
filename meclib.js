3a00b01cd544ecdb071802891cb3b75dfaebdc51
Niclas17 <105876203+Niclas17@users.noreply.github.com>
2023-01-05 16:32:54 +0100

    Update meclib.js

--- meclib.js
+++ meclib.js
@@ -1042,17 +1042,54 @@ class polygon {
       else {this.state = "locked"}
     this.loads = []; 
     this.d = data.slice(0);
-    this.v = data.slice(2);
-    this.p = board.create('polygon',this.v, {opacity: true, fillcolor:'lightgray', vertices:{size:0, fixed: true} ,borders: normalStyle, hasInnerPoints:true } );
-    // switching objects
-    this.obj = [this.p].concat(this.p.borders);
-    // state init
-    if (this.state == "show") { show(this) }
-    if (this.state == "hide") { hide(this) }
-    if (this.state != "locked") { makeSwitchable(this.p, this) }
-
+    this.v = data.slice(2); //Array mit den Vertices Arrays
+    this.p = [];  //Array für die Polygone
+    this.c = [];  //Array für die curved-Differenz-Objekte
+    this.obj = [];  //Array für die Objekte
+	  
+    for (let i = 0; i < this.v.length; i++) {
+    	let attributes = {opacity: true, fillcolor: 'lightgrey', vertices: {size: 0,fixed: true}, borders: normalStyle, hasInnerPoints: true};
+      	if (this.v.length > 1) {
+      		attributes.borders = {visible: false};
+      		attributes.fillcolor = 'none';
+    	}
+      
+   	this.p[i] = board.create('polygon', this.v[i], attributes);
+     	this.obj = this.obj.concat(this.p[i]).concat(this.p[i].borders);
+      
+      // Erstellen der Curved Differenz Objekte
+      // Derzeit keine möglichkeit mehr als eine Differenz zu erstellen, aber Möglichkeit
+      // offen gehalten
+      	if (i > 0) {
+     		this.c[i-1] = board.create('curvedifference', [this.p[0], this.p[i]], {
+       		strokeWidth: 2,
+       		fillColor: 'lightgrey',
+       		fillOpacity: 0.7,
+       		strokecolor: 'black'
+     		});
+     		this.obj = this.obj.concat(this.c[i-1]);
+   	}   
+    }
+    	// state init
+  	if (this.state == "show") { show(this) }
+  	if (this.state == "hide") { hide(this) }
+  	if (this.state != "locked") {
+    		if (this.v.length > 1){
+      			makeSwitchable(this.c[0], this);
+      		} else {
+      			makeSwitchable(this.p[0], this);
+      		}
+  	}
   }
-  hasPoint(pt) {return isOn(pt,this.p)} 
+  
+  hasPoint(pt) {
+   for (let i = 0; i < this.p.length; i++) {
+     if (isOn(pt, this.p[i])) {
+       return true;
+     }
+   }
+   return false;
+  }
   data(){ var a = this.d.slice(0); a.push(this.state); return a}
   name(){ return targetName(this) }  
 }
