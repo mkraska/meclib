45a27faf2d3e51c1d9626411d13c88bcb2a2945c
mkraska <kraska@th-brandenburg.de>
2024-09-27 17:25:10 +0200

    Update

--- meclib.js
+++ meclib.js
@@ -1,6 +1,6 @@
 // https://github.com/mkraska/meclib/wiki
 // version info
-const versionText= "JXG "+JXG.version+" iMeclib 2024 09 24";
+const versionText= "JXG "+JXG.version+" Meclib 2024 09 24";
 const highlightColor = "orange";
 const movableLineColor = "blue";
 const loadColor = "blue";
